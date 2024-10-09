252a5cc8a11a81e29d3591e75c7406c1982d367b
Matt Raffel <matt.raffel@gmail.com>
2024-10-09 07:49:38 -0600

    this is just a test
    Signed-off-by: Matt Raffel <matt.raffel@gmail.com>

--- src/cli/Program.cs
+++ src/cli/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+if (null == args || 0 == args.Length)
+{
+    Console.WriteLine("arguments are required");
+}
+else 
+{
+    Console.WriteLine("OK we got arguments");  
+}
+
+
+return 0;
