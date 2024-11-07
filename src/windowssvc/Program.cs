d93190987ed6ec77508f3603cfb831e2c79f3957
Matt Raffel <matt.raffel@gmail.com>
2024-11-07 12:32:58 -0700

    gots grpc hosting in
    Signed-off-by: Matt Raffel <matt.raffel@gmail.com>

--- src/windowssvc/Program.cs
+++ src/windowssvc/Program.cs
@@ -1,8 +1,19 @@
 using ArcusWinSvc;
+using Grpc.Core;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
-var builder = Host.CreateApplicationBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddWindowsService();
 builder.Services.AddHostedService<Worker>();
-
+builder.Services.AddGrpc().AddServiceOptions<ActionsServiceImpl>(options =>
+{
+    options.MaxReceiveMessageSize = 10 * 1024;
+});
 
 var host = builder.Build();
+host.MapGrpcService<ActionsServiceImpl>();
+
+// Run the host
 host.Run();
